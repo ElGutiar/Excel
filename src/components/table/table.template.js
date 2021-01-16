2e677a9790dc9bf12c72fda926dec705ce5bc972
ElGutiar <el12guitar@gmail.com>
2021-01-16 22:37:42 +0300

    Finish base framework functional

---
+++ src/components/table/table.template.js
@@ -0,0 +1,52 @@
+const CODES = {
+  A: 65,
+  Z: 90
+}
+
+const createRow = function(index, col) {
+  return `
+   <div class="row">
+      <div class="row-info">${index ? index : ''}</div>
+      <div class="row-data">${col}</div>
+  </div>
+  `
+}
+
+const toColumn = function(char) {
+  return `
+    <div class="column">${char}</div>
+ `
+}
+
+const toCell = function() {
+  return `
+    <div class="cell" contenteditable></div>
+  `
+}
+
+const goToChar = function(_, index) {
+  return String.fromCharCode(CODES.A + index)
+}
+
+export function createTable(rowsCount = 15) {
+  const columnCount = CODES.Z - CODES.A + 1
+  const rows = []
+
+  const columns = new Array(columnCount)
+      .fill('')
+      .map(goToChar)
+      .map(toColumn)
+      .join('')
+
+  rows.push(createRow(null, columns))
+
+  for (let i = 0; i < rowsCount; i++) {
+    const cell = new Array(columnCount)
+        .fill('')
+        .map(toCell)
+        .join('')
+    rows.push(createRow(i + 1, cell))
+  }
+
+  return rows.join('')
+}
