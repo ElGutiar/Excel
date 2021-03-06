1e38f9336a8b54571f493d63e82ff7b25e2c0e7f
ElGutiar <el12guitar@gmail.com>
2021-03-06 21:04:23 +0300

    refactoring dashboard. Added functionality to get items from storage

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,38 +1,26 @@
 import {$} from '@/core/dom'
 import {Page} from '@/core/Page'
+import {getAllRecords} from './dashboard.functions'
 
 export class DashboardPage extends Page {
   getRoot() {
+    const now = Date.now().toString()
     return $.create('div', 'db').html(`
+
       <div class="db__header">
         <h1>Excel Dashboard</h1>
       </div>
 
       <div class="db__new">
         <div class="db__view">
-          <a href="#" class="db__create">
+          <a href="#excel/${now}" class="db__create">
               Новая <br /> таблица
           </a>
         </div>
       </div>
-
+      
       <div class="db__table db__view">
-        <div class="db__list-header">
-            <span>Название</span>
-            <span>Дата открытия</span>
-        </div>
-
-      <ul class="db__list">
-        <li class="db__record">
-            <a href="#">Таблица номер 1</a>
-            <strong>12.06.20</strong>
-        </li> 
-
-        <li class="db__record">
-          <a href="#">Таблица номер 2</a>
-          <strong>12.06.20</strong>
-      </li> 
-      </ul>
+        ${getAllRecords()}
       </div>
     `)
   }
